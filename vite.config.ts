e57f78f18e1ba8e05d882b9eefd91e12403fbcc1
DemonRider <michaeldouglasa65@gmail.com>
2026-08-25 10:18:24 -0300

    Corrigir caminhos do GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/fichas-de-ameaca-t20/",
   server: {
     host: "localhost",
     port: 5173,
